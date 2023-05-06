98a22b874f7b43a6a48d243271da94e2fe66cde1
Najma Abdi <anajmabdi@gmail.com>
2023-05-05 19:55:01 -0700

    seed data for animal model, add gender property

--- AnimalShelterApi/Models/Animal.cs
+++ AnimalShelterApi/Models/Animal.cs
@@ -7,5 +7,6 @@ namespace AnimalShelterApi.Models
     public string Species { get; set; }
     public string Breed {get; set; }
     public int Age { get; set; }
+    public string Gender {get; set; }
   }
 }
--- AnimalShelterApi/Models/AnimalShelterApiContext.cs
+++ AnimalShelterApi/Models/AnimalShelterApiContext.cs
@@ -9,5 +9,19 @@ namespace AnimalShelterApi.Models
     public AnimalShelterApiContext(DbContextOptions<AnimalShelterApiContext> options) : base(options)
     {
     }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Animal>()
+        .HasData(
+          new Animal { AnimalId = 1, Name = "Oreo", Species = "Cat", Breed = "Tuxedo", Age = 2, Gender = "Female"},
+          new Animal { AnimalId = 2, Name = "Mr.Mittens", Species = "Cat", Breed = "Tabby", Age = 5, Gender = "Male"},
+          new Animal { AnimalId = 3, Name = "Patchouli", Species = "Cat", Breed = "Calico", Age = 6, Gender = "Female"},
+          new Animal { AnimalId = 4, Name = "Electra", Species = "Dog", Breed = "Great Pyrenees", Age = 4, Gender = "Male"},
+          new Animal { AnimalId = 5, Name = "Bentley", Species = "Dog", Breed = "German Shepherd", Age = 12, Gender = "Male"},
+          new Animal { AnimalId = 6, Name = "KitKat", Species = "Dog", Breed = "Beagle", Age = 7, Gender = "Female"}
+        );
+    }
+
   }
 }
